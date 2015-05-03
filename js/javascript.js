01e4d2744201d73f2d86535758a41cf29064768f
Haris Spahic <hspahic1@etf.unsa.ba>
2015-05-03 15:12:33 +0200

    Dodana validacija forme putem i ucitavanje podstranica putam AJAXa (1/2)
    Treci projektni zadatak/Treca spirala

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,13 @@
 
-window.onload= function(){
+window.onload = function () {
     //document.body.style.backgroundColor = "red";
-    
+   otvori("pages/index1.html");
    document.getElementById('femailcb').style.display="none";
     
     
-}
+} 
 var cekirano=false;
-
+var uspjesno=false;
 function promjeniboju(el){
 }
 
@@ -23,16 +23,24 @@ function showMenu(){
 
 
 function validacija(){
+	
+	
+	
     var ime= document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var text = document.getElementById('ta').value;
+	
     var femail= document.getElementById('femailcb').value;
+	
+	
+    
     if(!provjeriIme(ime)){
-        
         document.getElementById('name').style.background= "rgba(242, 158, 158, 0.58)";
         return false;
     }
-    
+	else{
+		document.getElementById('name').style.background= "transparent";	
+	}
     if(!provjeriEmail(email)){
         document.getElementById('email').style.background= "rgba(242, 158, 158, 0.58)";
         return false;
@@ -49,15 +57,27 @@ function validacija(){
     }
     else{
         document.getElementById('femailcb').style.background= "transparent";
+		}
     }
+    if(!uspjesno){
+        document.getElementById("city").style.background = "rgba(242, 158, 158, 0.61)";
+        document.getElementById("postal").style.background = "rgba(242, 158, 158, 0.61)";
+        
+        return false;
     }
-    
     
     if(!provjeriText(text)){
         document.getElementById('ta').style.background="rgba(242, 158, 158, 0.61)";
         return false;
     }
+    else{
+        document.getElementById('ta').style.background="transparent";
+    }
+    
+    
+        
     return true;
+    
 }
 
 function provjeriIme(i)
@@ -93,3 +113,52 @@ function kliknuto(cb){
     }
     
 }
+
+
+//-----------Provjera servisa ---------------//
+function f(){
+	
+var grad = document.getElementById("city");
+var pbroj = document.getElementById("postal");
+var OK= '{"ok":"Poštanski broj odgovara mjestu"}';
+var nepostojiBroj = '{"greska":"Nepostojeći poštanski broj"}';
+var nepostojiGrad = '{"greska":"Nepostojeće mjesto"}';
+var ajax = new XMLHttpRequest();
+    
+	ajax.onreadystatechange = function() {// Anonimna funkcija
+		if (ajax.readyState == 4 && ajax.status == 200)
+			if(ajax.responseText === OK){
+				grad.style.background = "rgba(148, 229, 148, 0.45)";
+				pbroj.style.background="rgba(148, 229, 148, 0.45)";
+                uspjesno=true;
+                
+			}		
+			else if(ajax.responseText === nepostojiGrad){
+				
+				grad.style.background = "rgba(242, 158, 158, 0.61)";
+                uspjesno=false;
+                
+				}
+			else{
+				grad.style.background = "rgba(148, 229, 148, 0.45)";
+				pbroj.style.background = "rgba(242, 158, 158, 0.61)";
+                uspjesno=false;
+			}
+	}
+	ajax.open("GET", "http://zamger.etf.unsa.ba/wt/postanskiBroj.php?mjesto="+grad.value+"&postanskiBroj="+pbroj.value, true);
+	ajax.send();
+}
+
+function otvori(stranica){
+	var ajax = new XMLHttpRequest();
+	ajax.onreadystatechange = function() {// Anonimna funkcija
+		if (ajax.readyState == 4 && ajax.status == 200)
+			document.getElementById("sadrzaj").innerHTML = ajax.responseText;
+		if (ajax.readyState == 4 && ajax.status == 404)
+			document.getElementById("sadrzaj").innerHTML = "Greska: nepoznat URL";
+	}
+	ajax.open("GET", stranica, true);
+	ajax.send();
+}
+
+
