7fc477b304e455755c9c43e82a209cf28cf21462
Haris <hspahic1@etf.unsa.ba>
2015-05-14 14:07:05 +0200

    Ispravke
    Opet neke ispravke

--- pages/news.php
+++ pages/news.php
@@ -10,16 +10,19 @@ foreach($files1 as $fajlovi){
 	{
 		$linije= file("novosti/".$fajlovi);
 		echo "<article class='tekstslika' >";
-		echo "<div class='tekst'>
-			<img src=".$linije[3]." alt='/'>
-			<h5>".$linije[0]."</h5>"
+		echo "<div class='tekst'>";
+		if($linije[3]!==""){
+			echo "<img src=".$linije[3]." alt='/'>";
+		}
+		
+			echo "<h5>".$linije[0]."</h5>"
 			."<h5>".$linije[1]."</h5>"
 			."<h2>".$linije[2]."</h2>";
 			foreach($linije as $kljuc=>$vrijednost){
 				if($kljuc>3){
-					echo "<p>".$vrijednost."</p>";
+				echo "<p>".$vrijednost."</p>";
+					}					
 				} 			
-			}
 			echo "</div>";
 			
 		echo "</article>";
@@ -27,6 +30,8 @@ foreach($files1 as $fajlovi){
 	}	
 
 }
+
+
 
 ?>
 <!--
